978b98888d2137248957314f920bb75c01a61f93
Yhonatan Jesus <yiquiapaza@gmail.com>
2020-07-16 00:06:19 -0300

    Style header and plot

--- src/app.js
+++ src/app.js
@@ -1,6 +1,9 @@
 import dataAlphabet from '../data/data.json';
 import { select, scaleLinear, max, scaleBand, axisLeft, axisBottom } from 'd3';
 
+import style from './style';
+
+style();
 const svg = select('svg');
 
 const width = +svg.attr('width');
@@ -19,7 +22,8 @@ const render = (data) => {
 
 	const yScale = scaleBand()
 		.domain(data.map((d) => yValue(d)))
-		.range([0, innerHeight]);
+		.range([0, innerHeight])
+		.padding(0.1);
 
 	const g = svg
 		.append('g')
@@ -30,13 +34,13 @@ const render = (data) => {
 		.call(axisBottom(xScale))
 		.attr('transform', `translate(0, ${innerHeight})`);
 
-	g.selectAll('rect')
+	g.selectAll('circle')
 		.data(data)
 		.enter()
-		.append('rect')
-		.attr('y', (d) => yScale(yValue(d)))
-		.attr('width', (d) => xScale(xValue(d)) * 10000)
-		.attr('height', yScale.bandwidth());
+		.append('circle')
+		.attr('cy', (d) => yScale(yValue(d)))
+		.attr('cx', (d) => xScale(xValue(d)) * 10000)
+		.attr('r', yScale.bandwidth());
 };
 
 render(dataAlphabet);
