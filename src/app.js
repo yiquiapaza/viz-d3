9c5e81d51123cc6e790740be0dd360c9cd6d0246
Yhonatan Jesus <yiquiapaza@gmail.com>
2020-08-23 01:53:54 -0300

    Refactor all project
    Add more countries and more year for each one

--- src/app.js
+++ src/app.js
@@ -1,263 +1,126 @@
-import dataAlphabet from '../data/data.json';
-import newData from '../data/newData.json';
+import data from '../data/newData.json';
 import './style.css';
 import {
 	select,
-	scaleLinear,
 	max,
-	scaleBand,
-	axisLeft,
-	axisBottom,
-	format,
 	min,
-	scaleOrdinal,
-	map,
-	scaleLog,
 	extent,
+	axisBottom,
+	axisLeft,
 	scaleSqrt,
+	scaleLinear,
+	keys,
 } from 'd3';
 
-const dimension_chartbar = document.querySelector('#dimension-chartbar');
-const augmented_chartbar = document.querySelector('#augmented-chartbar');
-
-const addDimesion = () => {
-	dataAlphabet.map((item) => {
-		item.time = Math.random() * 100;
-	});
-};
-
-const sendData = () => {
-	fetch('http://localhost:3030/data-json', {
-		method: 'POST',
-		headers: {
-			'Content-Type': 'application/json',
-		},
-		body: JSON.stringify(dataAlphabet),
-	})
-		.then((response) => {
-			return response.json();
-		})
-		.catch((error) => {
-			console.log(error);
-			throw new Error('Something went wrong');
-		});
-};
-
-dimension_chartbar.addEventListener('click', addDimesion);
-//augmented.addEventListener('click', sendData);
-augmented_chartbar.addEventListener('click', sendData);
-
-const barchart = select('#barchar')
-	.append('svg')
-	.attr('width', 1200)
-	.attr('height', 900);
-
-const scatterplot = select('#scatterplot')
-	.append('svg')
-	.attr('width', 1200)
-	.attr('height', 900);
-
-const width = +barchart.attr('width');
-const height = +barchart.attr('height');
-
-const renderBarchart = (data) => {
-	const xValue = (d) => d.value;
-	const yValue = (d) => d.name;
-	const margin = { top: 80, right: 80, bottom: 80, left: 80 };
-	const innerWidth = width - margin.left - margin.right;
-	const innerHeight = height - margin.top - margin.bottom;
-
-	const xScale = scaleLinear()
-		.domain([0, max(data, (d) => xValue(d)) * 10000])
-		.range([0, innerWidth]);
-
-	const yScale = scaleBand()
-		.domain(data.map((d) => yValue(d)))
-		.range([0, innerHeight])
-		.padding(0.1);
-
-	const g = barchart
-		.append('g')
-		.attr('transform', `translate(${margin.left}, ${margin.top})`);
-
-	g.append('g').call(axisLeft(yScale));
-	g.append('g')
-		.call(axisBottom(xScale))
-		.attr('transform', `translate(0, ${innerHeight})`);
-
-	g.selectAll('rect')
-		.data(data)
-		.enter()
-		.append('rect')
-		.attr('y', (d) => yScale(yValue(d)))
-		.attr('width', (d) => xScale(xValue(d)) * 10000)
-		.attr('height', yScale.bandwidth());
-};
-
-const renderScatterplot = (data) => {
-	const xValue = (d) => d.value;
-	const yValue = (d) => d.name;
-
-	const margin = { top: 80, right: 80, bottom: 80, left: 80 };
-	const innerWidth = width - margin.left - margin.right;
-	const innerHeight = height - margin.top - margin.bottom;
-
-	const xScale = scaleLinear()
-		.domain([0, max(data, (d) => xValue(d))])
-		.range([0, innerWidth]);
-
-	const yScale = scaleBand()
-		.domain(data.map((d) => yValue(d)))
-		.range([0, innerHeight])
-		.padding(0.1);
-
-	const g = scatterplot
-		.append('g')
-		.attr('transform', `translate(${margin.left}, ${margin.top})`);
-
-	const xAxisTickFormat = (number) => {
-		format('.3s')(number).replace('G', 'B');
-	};
-
-	const xAxis = axisBottom(xScale).tickSize(-innerHeight).tickPadding(15);
-
-	const yAxis = axisLeft(yScale).tickSize(-innerWidth).tickPadding(10);
-
-	const yAxisG = g.append('g').call(yAxis);
-	yAxisG.selectAll('.domain').remove();
-
-	yAxisG
-		.append('text')
-		.attr('class', 'axis-label')
-		.attr('y', -93)
-		.attr('x', -innerHeight / 2)
-		.attr('fill', 'black')
-		.attr('transform', `rotate(-90)`)
-		.attr('text-anchor', 'middle')
-		.text('Cantidad');
-
-	const xAxisG = g
-		.append('g')
-		.call(xAxis)
-		.attr('transform', `translate(0, ${innerHeight})`);
-
-	xAxisG.select('.domain').remove();
-	g.selectAll('circle')
-		.data(data)
-		.enter()
-		.append('circle')
-		.attr('cy', (d) => yScale(yValue(d)) + yScale.bandwidth() / 2)
-		.attr('cx', (d) => xScale(xValue(d)))
-		.attr('r', yScale.bandwidth() / 6)
-		.on('click', click);
-	function click(d) {
-		console.log(d); //considering dot has a title attribute
-	}
-};
-
-let final_width = 1200;
-let final_height = 900;
-
-let final_margin = {
-	top: 80,
-	right: 80,
-	bottom: 80,
-	left: 80,
-};
-
+let width = 1200;
+let height = 800;
+let margin = { top: 100, right: 100, bottom: 100, left: 100 };
 let colors = ['#596F7E', '#168B98', '#ED5B67', '#fd8f24', '#919c4c'];
+let minYear = min(data, (d) => d.year);
+let maxYear = max(data, (d) => d.year);
 
-let minYear = min(newData, (d) => d.year);
-let maxYear = max(newData, (d) => d.year);
-
-let colorScale = scaleOrdinal()
-	.domain(map(newData, (d) => d.region).keys())
-	.range(colors);
-
-const final_visualization = select('#final')
+const svg = select('#final')
 	.append('svg')
-	.attr('width', final_width)
-	.attr('height', final_height);
+	.attr('width', width)
+	.attr('height', height)
+	.style('fill', '#F5F5F2');
 
-final_visualization.append('g');
+const t = svg.transition().duration(2000);
+//const circle = svg;
 
-let xScale = scaleLog()
-	.domain(extent(newData, (d) => d.life_expectance))
+const xScale = scaleLinear()
+	.domain(extent(data, (d) => d.life_expectance))
 	.nice()
-	.range([final_margin.left, final_width - final_margin.right]);
+	.range([margin.left, width - margin.right]);
 
-let yScale = scaleLog()
-	.domain(extent(newData, (d) => d.infant_mortality_rate))
+const yScale = scaleLinear()
+	.domain(extent(data, (d) => d.infant_mortality_rate))
 	.nice()
-	.range([final_height - final_margin.bottom, final_margin.top]);
+	.range([height - margin.bottom, margin.top]);
 
-let aScale = scaleSqrt()
-	.domain(extent(newData, (d) => d.population))
-	.range([0, 30]);
-
-let xAxis = (g) =>
+const xAxis = (g) =>
 	g
-		.attr('transform', `translate(0, ${final_height - final_margin.bottom})`)
+		.attr('transform', `translate(0, ${height - margin.bottom})`)
 		.call(axisBottom(xScale))
 		.call((g) => g.select('.domain').remove());
 
 let yAxis = (g) =>
 	g
-		.attr('transform', `translate(${final_margin.left}, 0)`)
+		.attr('transform', `translate(${margin.left}, 0)`)
 		.call(axisLeft(yScale))
 		.call((g) => g.select('.domain').remove());
 
-const t = final_visualization.transition().duration(1000);
+let aScale = scaleSqrt()
+	.domain([0, max(data, (d) => d.population)])
+	.range([0, 20]);
 
-const out = final_visualization.select('g');
+svg.append('g').call(xAxis);
+svg.append('g').call(yAxis);
 
-out
-	.selectAll('circle')
-	.data(newData, (d) => d.name)
-	.join(
-		(enter) =>
-			enter
-				.append('circle')
-				.attr('class', 'bubble')
-				.attr('id', (d, i) => 'point-' + i)
-				.attr('fill-opacity', 0)
-				.attr('fill', (d) => colorScale(d.region))
-				.attr('cx', final_width / 2 - final_margin.left)
-				.attr('cy', final_height / 2 - final_margin.bottom)
-				.attr('r', 0)
-				.call((enter) =>
-					enter
-						.transition(t)
-						.attr('fill-opacity', 0.75)
-						.attr('cx', (d) => xScale(d.life_expectance))
-						.attr('cy', (d) => yScale(d.infant_mortality_rate))
-						.attr('r', (d) => aScale(d.population))
-				),
-		(update) =>
-			update.call((update) =>
+const createScatterplot = (_data) => {
+	svg
+		.selectAll('circle')
+		.data(_data)
+		.join(
+			(enter) =>
+				enter
+					.append('circle')
+					.attr('class', 'bubble')
+					.attr('fill-opacity', 0)
+					.attr('fill', (d) => d.color)
+					.attr('id', (d, i) => 'point-' + i)
+					.attr('cx', width / 2 - margin.left)
+					.attr('cy', height / 2 - margin.bottom)
+					.attr('r', 0)
+					.on('click', (d) => {
+						console.log(d.name);
+						console.log(d.life_expectance);
+						console.log(d.infant_mortality_rate);
+						console.log(d.population);
+					})
+					.call((enter) =>
+						enter
+							.transition(t)
+							.attr('fill-opacity', 0.75)
+							.attr('fill', (d) => d.color)
+							.attr('cx', (d) => xScale(d.life_expectance))
+							.attr('cy', (d) => yScale(d.infant_mortality_rate))
+							.attr('r', (d) => aScale(d.population))
+					),
+			(update) =>
 				update
-					.transition(t)
+					.call((update) => update.transition(t))
+					.attr('fill', (d) => d.color)
 					.attr('cx', (d) => xScale(d.life_expectance))
 					.attr('cy', (d) => yScale(d.infant_mortality_rate))
 					.attr('r', (d) => aScale(d.population))
-			),
-		(exit) =>
-			exit.call((exit) =>
-				exit.transition(t).attr('r', 0).style('fill-opacity', 0).remove()
-			)
-	);
+					.on('click', (d) => {
+						console.log(d.name);
+						console.log(d.life_expectance);
+						console.log(d.infant_mortality_rate);
+						console.log(d.population);
+					}),
+			(exit) =>
+				exit.call((exit) =>
+					exit.transition(t).attr('r', 0).style('fill-opacity', 0).remove()
+				)
+		);
 
-final_visualization
-	.append('text')
-	.attr(
-		'transform',
-		'translate(' + final_width / 2 + ',' + (final_height - 20) + ')'
-	)
-	.style('text-anchor', 'middle')
-	.style('font-size', '20px')
-	.text('Life Expectance Both');
+	console.log('se ejecuto');
+};
 
-final_visualization.append('g').call(xAxis);
-final_visualization.append('g').call(yAxis);
-renderBarchart(dataAlphabet);
-renderScatterplot(dataAlphabet);
+let year_data = data.filter((obj) => {
+	return obj.year === 1950;
+});
+
+createScatterplot(year_data);
+select('#year').on('input', function () {
+	let year_data = data.filter((obj) => {
+		return obj.year === +this.value;
+	});
+
+	svg.enter();
+	createScatterplot(year_data);
+	console.log(year_data);
+	console.log(+this.value);
+});
