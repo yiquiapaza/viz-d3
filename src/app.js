b2fa1bdd8dba64b43248a30e967852a7ef63ccf8
Yhonatan Jesus <yiquiapaza@gmail.com>
2021-08-31 12:04:30 -0300

    Add a progress bar with interval time

--- src/app.js
+++ src/app.js
@@ -2,6 +2,23 @@ import './style.css';
 import data from '../data/first_expriment.json';
 import { createScatterPlot } from './visualizations';
 
+let button = document.getElementById('button');
+button.onclick = () => {
+	let temp = 0;
+	setInterval(() => {
+		if(temp > 24){
+			temp = 0;
+			document.getElementById('myBar').style.width = '0%';
+		}
+		else{
+			document.getElementById('myBar').style.width = (temp * 4) +'%';
+			console.log(temp);
+			temp++;
+		}
+		}, 500);
+}
+
+
 createScatterPlot(
 	'#scatter--plot--visualization',
 	data,
