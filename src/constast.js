6bd651ed1c17a273c82f472f020afc121e6aff75
Yhonatan Jesus <yiquiapaza@gmail.com>
2022-02-21 14:22:33 -0300

    global refactor

--- src/constast.js
+++
@@ -1,5 +0,0 @@
-export const BREAKPOINT_DESKTOP = 2560;
-export const BREAKPOINT_LATOP = 1024;
-export const BREAKPOINT_TABLET = 768;
-export const BREAKPOINT_MOBILEM = 375;
-export const BREAKPOINT_MOBILEL = 425;
